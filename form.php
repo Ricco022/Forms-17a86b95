05eee31018f05a4873924b4e59bfb944d6381a0c
Riccardo van der Horst <riccardo271002@gmail.com>
2019-11-28 11:57:53 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="#">
+    <label class="login" for="name"></label>
+    <input type="Email" id="mail" name="mail" class="login" placeholder="Voer je e-mailadres in">
+    <button type="button">Vestuur</button>
+</form>
+</body>
+</html>
